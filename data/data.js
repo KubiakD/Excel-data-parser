927d87644d65c45cc299a63946884171814521d8
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-26 15:26:19 +0200

    add mongoose with question schema and model

--- data/data.js
+++ data/data.js
@@ -1,26 +1,10 @@
 const mongoose =  require('mongoose');
 
-const easyQuestionSchema = new mongoose.Schema({
+const questionsSchema = new mongoose.Schema({
     question: String,
     answers: [String],
-    correctAnswer: String
+    correctAnswer: String,
+    difficultyLevel: String
 });
-const mediumQuestionSchema = new mongoose.Schema({
-    question: String,
-    answers: [String],
-    correctAnswer: String
-});
-const hardQuestionSchema = new mongoose.Schema({
-    question: String,
-    answers: [String],
-    correctAnswer: String
-});
-
-const easyQuestionModel = mongoose.model('Easy', easyQuestionSchema);
-const mediumQuestionModel = mongoose.model('Medium', mediumQuestionSchema);
-const hardQuestionModel = mongoose.model('Hard', hardQuestionSchema);
-module.exports = {
-    Easy: easyQuestionModel,
-    Medium: mediumQuestionModel,
-    Hard: hardQuestionModel
-};
+const Question = mongoose.model('Question', questionsSchema);
+module.exports = Question;
