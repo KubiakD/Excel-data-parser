cdf18485805d6b4154257885d0ece0b37fb35d55
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-26 16:31:17 +0200

    add checking if read questions already exist in the db

--- app.js
+++ app.js
@@ -26,7 +26,17 @@ const readAndSave = async () => {
         };
         modifiedQuestionArr.push(modifiedObj);
     })
-    for( const question of modifiedQuestionArr) {
+    const existingQuestions = await Question.find({},{'_id':0,'question':1});
+    const newQuestions = modifiedQuestionArr.filter(question => {
+        return !existingQuestions.some(existingQuestion => {
+            return existingQuestion.question === question.question
+        });
+    });
+    if (!newQuestions || newQuestions.length === 0) {
+        console.log('No new questions!');
+        return
+    }
+    for(const question of newQuestions) {
                 const newQuestion = new Question(question);
                 await newQuestion.save();
                 console.log('Question saved!')
