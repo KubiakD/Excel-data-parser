60db6b8c4836fff0c0ed37e504221da517cd8d8d
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-04-26 15:27:13 +0200

    connect app.js with db and save parsed questions into it

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const xlsx = require('xlsx');
 const file = xlsx.readFile('./LoreQuiz.xlsx');
-for (const sheet in file.Sheets) {
-    const currentSheet = file.Sheets[sheet]
-    const questionsArr = xlsx.utils.sheet_to_json(currentSheet);
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Question = require('./data/data');
+
+const readAndSave = async () => {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('db connected!')
+    for (const sheet in file.Sheets) {
+        const currentSheet = file.Sheets[sheet]
+        const questionsArr = xlsx.utils.sheet_to_json(currentSheet);
     let modifiedQuestionArr = [];
     questionsArr.map(questionObj => {
         let answers = []
@@ -14,8 +21,16 @@ for (const sheet in file.Sheets) {
         const modifiedObj = {
             question: questionObj.question,
             answers,
-            correctAnswer: questionObj.correctAnswer
+            correctAnswer: questionObj.correctAnswer,
+            difficultyLevel: questionObj.difficultyLevel
         };
         modifiedQuestionArr.push(modifiedObj);
     })
-}
+    for( const question of modifiedQuestionArr) {
+                const newQuestion = new Question(question);
+                await newQuestion.save();
+                console.log('Question saved!')
+    }};
+    return console.log('Saving finished!')
+};
+readAndSave();
