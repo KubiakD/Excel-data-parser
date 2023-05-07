44c31d26becb6b146fc0d42eaa6003d4a652cf67
Daniel Kubiak <kubiakdaniel97@gmail.com>
2023-05-07 13:04:01 +0200

    add deleting questions from db when they are not in read file

--- checkAndUpdate.js
+++ checkAndUpdate.js
@@ -7,9 +7,12 @@ const Question = require('./data/data');
 const checkAndUpdate = async () => {
   await mongoose.connect(process.env.MONGO_URI);
   console.log('db connected!');
+  // fetch questions from db
+  const existingQuestions = await Question.find({},{'_id':0, '__v':0}).lean();
   for (const sheet in file.Sheets) {
     const currentSheet = file.Sheets[sheet];
     const questionsArr = xlsx.utils.sheet_to_json(currentSheet);
+    // transform answers into array 
     let modifiedQuestionArr = [];
     questionsArr.map((questionObj) => {
       let answers = [];
@@ -27,22 +30,30 @@ const checkAndUpdate = async () => {
       };
       modifiedQuestionArr.push(modifiedObj);
     });
-    const existingQuestions = await Question.find({},{'_id':0, '__v':0}).lean();
-    let questionsForUpdate = [];
-    let questionsForDeleting = [];
+    // filter out questions for update 
+    const filteredExistingQuestions = existingQuestions.filter(question => question.difficultyLevel === sheet);
+    const questionsForUpdate = [];
     for (const question of modifiedQuestionArr){
-      const dbCounterpart = existingQuestions.find(q=>question.index === q.index);
+      const dbCounterpart = filteredExistingQuestions.find(q=>question.index === q.index);
       if(JSON.stringify(dbCounterpart)!== JSON.stringify(question)) {
-        questionsForUpdate.push(question);
-      } else if (!dbCounterpart || dbCounterpart.length === 0) {
-        questionsForDeleting.push(question);
+       questionsForUpdate.push(question);
       } else {
         console.log('No changes detected');
       };
-    }
+    };
+    // filter out questions for delete 
+    const existingQuestionsIndexArr = filteredExistingQuestions.map(question => question.index);
+    const readQuestionsIndexArr = modifiedQuestionArr.map(question => question.index);
+    const questionIndexesToRemove = existingQuestionsIndexArr.filter(index => !readQuestionsIndexArr.includes(index)); 
+    // update questions in db 
     for (const question of questionsForUpdate) {
         await Question.updateOne({index: question.index}, {$set: {...question}});
-        console.log(`Question ${question.question} updated!`)
+        console.log(`Question ${question.question} updated!`);
+    };
+    // delete questions in db 
+    for (const index of questionIndexesToRemove) {
+      await Question.deleteOne({index});
+      console.log(`Question ${index} deleted!`);
     };
   };
 };
